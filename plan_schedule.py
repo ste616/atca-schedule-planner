1f2c0eb323528dffca47a7b18a5fa6272af9b8f6
Jamie Stevens <ste616@gmail.com>
2017-11-04 13:31:24 +1100

    Fix a bug with outputting the wrong type of RA and Dec to the files.

--- plan_schedule.py
+++ plan_schedule.py
@@ -14,6 +14,9 @@ import json
 import sys
 import math
 import argparse
+import os
+import cabb_scheduler as cabb
+import rapid_library.routines as rapidlib
 
 # A class to hold information about a required scan.
 class sourceObservation:
@@ -83,7 +86,7 @@ def createSource(name=None, rightAscension=None, declination=None):
         fixedBody._ra = rightAscension
         fixedBody._dec = declination
         fixedBody.name = name
-        fixedBody.compute()
+        fixedBody.compute(epoch='2000')
         return fixedBody
     return None
 
@@ -142,7 +145,7 @@ def programEntry(source=None, observatory=None, startTime=None, duration=None):
     if source is not None and observatory is not None and startTime is not None and duration is not None:
         observatory.date = startTime
         source.compute(observatory)
-        entry = { 'name': source.name, 'rightAscension': source.ra, 'declination': source.dec,
+        entry = { 'name': source.name, 'rightAscension': source.a_ra, 'declination': source.a_dec,
                   'start': { 'time': startTime.datetime().strftime("%Y/%m/%d %H:%M:%S"),
                              'az': (source.az * 180.0 / math.pi), 'el': (source.alt * 180.0 / math.pi) }
         }
@@ -189,6 +192,15 @@ parser.add_argument('-S', "--slewing", default=1.0, type=float,
                     help="cannot have two seed sources within this slewing time of each other")
 parser.add_argument('-A', "--adjacent", default=4.0, type=float,
                     help="cannot have seeds in two adjacent segments further apart than this slewing time")
+parser.add_argument('-m', "--mosaic", default="seg",
+                    help="the prefix to use for the mosaic file names")
+parser.add_argument('-P', "--project", default="C3214",
+                    help="the project code to use in the schedule file")
+parser.add_argument('-o', "--output", default="test.sch",
+                    help="the name of the schedule file to output")
+parser.add_argument('-a', "--altered", default="",
+                    help="the name of the file to output with the list of sources, excluding the ones successfully scheduled")
+
 args = parser.parse_args()
 
 if args.sourcelist == "":
@@ -242,8 +254,10 @@ for i in xrange(0, len(sourceList)):
             badSources.append(i)
 
 print "Found %d sources that will not be above the horizon in your slot." % len(badSources)
+removedSources = []
 for i in xrange(len(sourceList) - 1, -1, -1):
     if i in badSources:
+        removedSources.append(sourceList[i])
         del sourceList[i]
 print "There are now %d sources remaining." % len(sourceList)
 
@@ -300,6 +314,7 @@ for i in xrange(0, len(sourceList)):
 print "Found %d sources that will not be above the horizon long enough." % len(badSources)
 for i in xrange(len(sourceList) - 1, -1, -1):
     if i in badSources:
+        removedSources.append(sourceList[i])
         del sourceList[i]
 print "There are now %d sources remaining." % len(sourceList)
 
@@ -394,19 +409,229 @@ for i in xrange(0, len(segmentSeeds)):
     else:
         print "Segment %d is seeded by %s" % ((i + 1), segmentSeeds[i].name)
     
-sys.exit(0)
+# Next step is to find the sources nearby to each seed source in each segment.
+# We use the threshold that allows for 15% overhead. That is, say we have a 15 min
+# segment, we must spend 85% of that time on source, so only 15% can be spent
+# slewing. That then limits the distance around the seed that we can search.
+# It also sets the number of sources we must have.
+integrationPerSegment = 0.85 * halfSpacing * (60. * 24.) # in minutes
+maxSlewTime = 0.15 * halfSpacing * (60. * 24.)
+nSourcesPerSegment = int(math.floor(integrationPerSegment / fullDuration))
+lastSource = None
+segmentOrdered = []
+segmentEstimates = []
+seedAssociations = {}
+for i in xrange(0, len(segmentSeeds)):
+    # For each segment, sort the sources by increasing slew time from
+    # the seed source, and select the closest nSourcesPerSegment.
+    if segmentSeeds[i] is None:
+        segmentOrdered.append(None)
+        segmentEstimates.append(None)
+        continue
+    print "Segment %d:" % i
+    segmentOrdered.append([])
+    seedPosition = timeToPosition(source=segmentSeeds[i], observatory=atca, time=segmentTimes[i])
+    segmentSlewTimes = []
+    if segmentSeeds[i].name in seedAssociations:
+        # We assume we continue using the same sources as before.
+        for j in xrange(0, len(seedAssociations[segmentSeeds[i].name])):
+            sourcePosition = timeToPosition(source=seedAssociations[segmentSeeds[i].name][j],
+                                            observatory=atca, time=segmentTimes[i])
+            segmentSlewTimes.append((seedAssociations[segmentSeeds[i].name][j],
+                                     calcSlewTime(seedPosition, sourcePosition)))
+    else:
+        for j in xrange(0, len(segmentSources[i])):
+            if segmentSeeds[i].name != segmentSources[i][j].name:
+                sourcePosition = timeToPosition(source=segmentSources[i][j], observatory=atca, time=segmentTimes[i])
+                segmentSlewTimes.append((segmentSources[i][j], calcSlewTime(seedPosition, sourcePosition)))
+    sortedSlewTimes = sorted(segmentSlewTimes, key=lambda s: s[1])
+    #print "Slew times around seed %s (%s %s)" % (segmentSeeds[i].name, segmentSeeds[i].a_ra,
+    #                                             segmentSeeds[i].a_dec)
+    tstore = {}
+    tstore[segmentSeeds[i].name] = segmentSeeds[i]
+    for j in xrange(0, len(sortedSlewTimes)):
+        #print "  Source %s (%s %s), slew time is %.2f mins" % (sortedSlewTimes[j][0].name,
+        #                                                       sortedSlewTimes[j][0].a_ra,
+        #                                                       sortedSlewTimes[j][0].a_dec,
+        #                                                       (sortedSlewTimes[j][1] / 60.))
+        tstore[sortedSlewTimes[j][0].name] = sortedSlewTimes[j][0]
+    # Now we take the necessary number of sources, and output them to a file
+    # so we can run them through atmos.
+    totalSlewTime = 0.
+    with open('temp_atmos.txt', 'w') as opf:
+        ostring = "%s %s %s\n" % (segmentSeeds[i].name, segmentSeeds[i].a_ra,
+                                  segmentSeeds[i].a_dec)
+        opf.write(ostring)
+        for j in xrange(0, min((nSourcesPerSegment - 1), len(sortedSlewTimes))):
+            if totalSlewTime > maxSlewTime:
+                break
+            ostring = "%s %s %s\n" % (sortedSlewTimes[j][0].name,
+                                      sortedSlewTimes[j][0].a_ra,
+                                      sortedSlewTimes[j][0].a_dec)
+            opf.write(ostring)
+            totalSlewTime += (sortedSlewTimes[j][1] / 60.)
+            print "  Source %s (%s %s), slew time is %.2f mins, total %.2f / %.2f mins" % (sortedSlewTimes[j][0].name,
+                                                                                           sortedSlewTimes[j][0].a_ra,
+                                                                                           sortedSlewTimes[j][0].a_dec,
+                                                                                           (sortedSlewTimes[j][1] / 60.),
+                                                                                           totalSlewTime, maxSlewTime)
+    # Now run atmos.
+    refpos=""
+    if lastSource is not None:
+        refpos = "ref=%s,%s,%s" % (lastSource.name, lastSource.a_ra,
+                                   lastSource.a_dec)
+    atca.date = segmentTimes[i]
+    atmosCommand = "atmos source=temp_atmos.txt out=%s%d.mos %s lst=%s interval=%d cycles=%d> /dev/null" % (args.mosaic, i, refpos, atca.sidereal_time(), int(args.cycletime), int((args.duration * 60.) / args.cycletime))
+    os.system(atmosCommand)
+    # Read in the order it was sorted into.
+    segstart = ""
+    segend = ""
+    segra = ""
+    segdec = ""
+    seedAssociations[segmentSeeds[i].name] = []
+    with open('%s%d.mos' % (args.mosaic, i), 'r') as ipf:
+        iline = ipf.readline()
+        while (iline != ""):
+            if iline.startswith("#") == False:
+                linel = [ x.strip() for x in iline.split(" ") ]
+                sname = linel[-1].replace('$', '')
+                segmentOrdered[i].append(tstore[sname])
+                if sname != segmentSeeds[i].name:
+                    seedAssociations[segmentSeeds[i].name].append(tstore[sname])
+                lastSource = tstore[sname]
+            else:
+                linel = [ x.strip() for x in iline.split(" ") ]
+                if iline.startswith("# Start LST is"):
+                    segstart = linel[4]
+                elif iline.startswith("# End LST is"):
+                    segend = linel[4]
+                elif iline.startswith("# Reference position ="):
+                    segra = linel[4]
+                    segdec = linel[5]
+            iline = ipf.readline()
+    print "ordered source list"
+    segmentEstimates.append({'startLST': segstart, 'endLST': segend,
+                             'refRA': segra, 'refDec': segdec})
+    for j in xrange(0, len(segmentOrdered[i])):
+        print " src %d: %s %s %s" % ((j + 1), segmentOrdered[i][j].name,
+                                     segmentOrdered[i][j].a_ra,
+                                     segmentOrdered[i][j].a_dec)
+            
+# Create a CABB schedule.
+schedule = cabb.schedule()
+atca.date = startDate
+# Which source do we start on?
+source1934 = createSource(name="1934-638", rightAscension="19:39:25.026",
+                          declination="-63:42:45.63")
+source0823 = createSource(name="0823-500", rightAscension="08:25:26.869",
+                          declination="-50:10:38.49")
+source1934.compute(atca)
+el1934 = source1934.alt * 180. / math.pi
+startSource = None
+if el1934 >= args.minel:
+    # We start on 1934-638.
+    startSource = source1934
+else:
+    # We start on 0823-500.
+    startSource = source0823
+schedule.addScan({
+    'source': startSource.name, 'rightAscension': startSource.a_ra,
+    'declination': startSource.a_dec, 'freq1': 5428, 'freq2': 7500,
+    'project': args.project, 'scanLength': rapidlib.minutesToScanLength(5),
+    'scanType': "Dwell"
+})
+nvisits = {}
+print "schedule estimated times follow:"
+totalSourceVisits = 0
+for i in xrange(0, len(segmentSeeds)):
+    if segmentSeeds[i] is None:
+        continue
+    schedule.addScan({
+        'source': "%s%d" % (args.mosaic, i),
+        'rightAscension': segmentEstimates[i]['refRA'],
+        'declination': segmentEstimates[i]['refDec'],
+        'scanType': "Mosaic", 'scanLength': rapidlib.minutesToScanLength(1)
+    })
+    print "  segment %d: LST %s - %s, %d sources" % (i, segmentEstimates[i]['startLST'],
+                                                     segmentEstimates[i]['endLST'],
+                                                     len(segmentOrdered[i]))
+    totalSourceVisits += len(segmentOrdered[i])
+    for j in xrange(0, len(segmentOrdered[i])):
+        if segmentOrdered[i][j].name not in nvisits:
+            nvisits[segmentOrdered[i][j].name] = 1
+        else:
+            nvisits[segmentOrdered[i][j].name] += 1
+        #schedule.addScan({
+        #    'source': segmentOrdered[i][j].name, 'rightAscension': segmentOrdered[i][j].a_ra,
+        #    'declination': segmentOrdered[i][j].a_dec
+        #})
 
-# Now start the program.
-program = []
-for i in xrange(0, len(sourceList)):
-    if sourceList[i].name == seedSource:
-        seedProgram = []
-        riseTime = atca.next_rising(sourceList[i], start=startDate)
-        seedProgram.append(programEntry(sourceList[i], atca, riseTime, fullDuration))
-        for j in xrange(1, args.nvisits):
-            ntime = ephem.Date(ephem.Date(seedProgram[-1]['end']['time']) + args.spacing / (24. * 60.))
-            seedProgram.append(programEntry(sourceList[i], atca, ntime, fullDuration))
-        program += seedProgram
-# Print out the program.
-for i in xrange(0, len(program)):
-    print program[i]
+print "Total number of source visits = %d" % totalSourceVisits
+visitSummary = {}
+for src in nvisits:
+    print "Source %s is visited %d times" % (src, nvisits[src])
+    if nvisits[src] not in visitSummary:
+        visitSummary[nvisits[src]] = 1
+    else:
+        visitSummary[nvisits[src]] += 1
+    if nvisits[src] >= args.nvisits:
+        # We can delete this source from the list.
+        for i in xrange(0, len(sourceList)):
+            if sourceList[i].name == src:
+                del sourceList[i]
+                break
+nn = 1
+while nn in visitSummary:
+    print "%d sources are visited %d times" % (visitSummary[nn], nn)
+    nn += 1
+    
+
+# Save the schedule.
+if args.output.endswith(".sch") == False:
+    args.output = args.output + ".sch"
+schedule.write(name=args.output)
+
+# Make a listing of the schedule.
+#listing = rapidlib.scheduleListing(schedule, atca, startDate)
+#print "schedule listing follows:"
+#nvisits = {}
+#for i in xrange(0, len(listing)):
+#    sname = schedule.getScan(i).getSource()
+#    if sname not in nvisits:
+#        nvisits[sname] = 1
+#    else:
+#        nvisits[sname] += 1
+#    print "Source %s: from %s - %s, el %.1f (visit %d)" % (sname,
+#                                                           listing[i]['start']['time'],
+#                                                           listing[i]['end']['time'],
+#                                                           listing[i]['start']['el'],
+#                                                           nvisits[sname])
+
+# Output the altered list if asked to.
+if args.altered != "":
+    if args.altered.endswith(".json"):
+        outObj = { 'sources': [] }
+        for i in xrange(0, len(sourceList)):
+            outObj['sources'].append({ 'name': sourceList[i].name,
+                                       'rightAscension': sourceList[i].a_ra,
+                                       'declination': sourceList[i].a_dec })
+        for i in xrange(0, len(removedSources)):
+            outObj['sources'].append({ 'name': removedSources[i].name,
+                                       'rightAscension': removedSources[i].a_ra,
+                                       'declination': removedSources[i].a_dec })
+        with open(args.altered, 'w') as ofp:
+            json.dump(outObj, ofp)
+    else:
+        with open(args.altered, 'w') as ofp:
+            for i in xrange(0, len(sourceList)):
+                oline = "%s%s%s%s%s\n" % (sourceList[i].name, args.csvdelim,
+                                          sourceList[i].a_ra, args.csvdelim,
+                                          sourceList[i].a_dec)
+                ofp.write(oline)
+            for i in xrange(0, len(removedSources)):
+                oline = "%s%s%s%s%s\n" % (removedSources[i].name, args.csvdelim,
+                                          removedSources[i].a_ra, args.csvdelim,
+                                          removedSources[i].a_dec)
+                ofp.write(oline)
+
+    
